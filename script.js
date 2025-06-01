71df0bf20bd27020c6b9f8f3a582d0233b79a171
Pezhman-Azizi <80008463+Pezhman-Azizi@users.noreply.github.com>
2025-06-01 16:12:17 +0100

    109-distructuring-data-structure

--- script.js
+++ script.js
@@ -71,3 +71,52 @@ console.log(main, secondary); // Italian Pizzeria
 console.log(main, secondary); // Italian Vegetarian
 */
 
+// ----------------------------------------------- Switching variables
+/*
+[main, secondary] = [secondary, main];
+console.log(main, secondary); // Vegetarian Italian
+*/
+// ----------------------------------------------- returning two values from a function
+/*
+console.log(restaurant.order(0, 1));
+//since an array is returned then we can destruct it. see the following:
+let [starter, mainCourse] = restaurant.order(0, 1);
+console.log(starter, mainCourse);
+
+*/
+
+// ----------------------------------------------- nested destructuring
+
+// const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+// const [l, , [m, n]] = nested;
+// console.log(l, m, n);
+
+// Destructuring Arrays:
+
+//----------------- 1.1:
+// Destructure the books array into two variables called firstBook and secondBook.
+
+// const [firstBook, secondBook] = books
+
+//----------------- 1.2
+// Destructure the books array into a variable called thirdBook. You must skip the first two books.
+
+// const [, , thirdBook] = books;
+
+
+//----------------- 1.3
+// Below is the nested ratings array that contains two other arrays. Destructure the nested ratings arrays into two variables called rating and ratingsCount. In the result of your destructuring, the ratings variable should store a number 4.19, and the ratingsCount variable should store a number 144584.
+
+// const ratings = [['rating', 4.19], ['ratingsCount', 144584]];
+
+// const [[,rating], [,ratingsCount]] = ratings;
+// console.log(rating, ratingsCount);
+
+// 1.4
+// Below is the ratingStars array. Destructure it into three variables called fiveStarRatings, oneStarRatings and threeStarRatings. Assign the threeStarRatings variable with a default value of 0.
+// const ratingStars = [63405, 1808];
+// const [fiveStarRatings, oneStarRatings, threeStarRatings=0] = ratingStars;
+// console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
+
